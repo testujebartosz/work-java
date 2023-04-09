0b069cda858f535b994603d20be082c8855a94ba
bkoldziej <testuje.bartosz@gmail.com>
2023-04-09 21:16:33 +0200

    ok

--- src/main/java/Komputer.java
+++ src/main/java/Komputer.java
@@ -1,6 +1,8 @@
 import org.apache.commons.lang3.StringUtils;
 
 import java.awt.*;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.List;
 
 public class Komputer {
@@ -10,6 +12,15 @@ public class Komputer {
  private List<Key> keysList;
  private boolean isActive;
 
+ public static void main(String[] args) {
+  BigDecimal n = new BigDecimal("1.659090");
+  System.out.println(n.setScale(2, RoundingMode.DOWN));
+  System.out.println(n.setScale(2, RoundingMode.FLOOR));
+  System.out.println(n.setScale(2, RoundingMode.CEILING));
+  System.out.println(n.setScale(2, RoundingMode.HALF_DOWN));
+  System.out.println(n.setScale(2, RoundingMode.HALF_UP));
+  System.out.println(n.setScale(2, RoundingMode.UP));
+ }
  public Komputer(int lenght, Color color, double price){
   this.lenght = lenght;
   this.color = color;
